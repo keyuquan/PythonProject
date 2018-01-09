1ebe2b880371cf63a530eceb890cbda65892559a
柯泉 <keq@yyft.com>
2018-01-09 20:14:26 +0800

    regression

--- c07_machinelearn/examples/c01_regression/__init__.py
+++ c07_machinelearn/examples/c01_regression/__init__.py
@@ -23,7 +23,8 @@ p =L(k1*x+k2*x+..+e),L=1/(1+e^(-y)) => p=1/(1+e^(-(k1*x+k2*x+..+e)))
  https://jingyan.baidu.com/article/63acb44ac60d4e61fcc17e2e.html
 * 二项式分布
 https://baike.baidu.com/item/%E4%BA%8C%E9%A1%B9%E5%88%86%E5%B8%83/1442377?fr=aladdin&fromid=3565421&fromtitle=%E4%BA%8C%E9%A1%B9%E5%BC%8F%E5%88%86%E5%B8%83
-* 正态分布
+* 期望,方差 正态分布
+https://wenku.baidu.com/view/898af692dd88d0d233d46a62.html
 https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=2&tn=baiduhome_pg&wd=%E6%AD%A3%E6%80%81%E5%88%86%E5%B8%83&rsv_spt=1&oq=%25E6%2599%25BA%25E8%2581%2594&rsv_pq=d208ecf600049ed1&rsv_t=e900CBzvOlNi08HwZYqwb%2Bz%2BKObaP3wvIVvLbtfgqW3T22p1%2BE7STpfsK9x4sw%2Bf4yEO&rqlang=cn&rsv_enter=1&inputT=7892&rsv_sug3=19&rsv_sug1=18&rsv_sug7=101&rsv_sug2=0&rsv_sug4=9506&rsv_sug=1
 * sigmod函数
  https://baike.baidu.com/item/Sigmoid%E5%87%BD%E6%95%B0/7981407?fr=aladdin
