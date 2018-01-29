2646366fe3827ca7aa85f9209f3792dccd08efe0
柯泉 <keq@yyft.com>
2018-01-29 18:27:31 +0800

    c03_bayes

--- c07_machinelearn/examples/c03_bayes/c01_bayes_spell_checker.py
+++ c07_machinelearn/examples/c03_bayes/c01_bayes_spell_checker.py
@@ -7,15 +7,18 @@
 """
 import re, collections
 
+
 # 把语料中的单词全部抽取出来, 转成小写, 并且去除单词中间的特殊符号
 def words(text): return re.findall('[a-z]+', text.lower())
 
-# 统计各个词语出现的次数
+
+# 统计已知文本 各个词语出现的次数
 def train(features):
     model = collections.defaultdict(lambda: 1)
     for f in features:
         model[f] += 1
     return model
+
 
 def edits1(word):
     n = len(word)
@@ -27,18 +30,20 @@ def edits1(word):
 
 
 # 找出 抽取 的词语中 包含已输入字符串 的词语
-def known(words,NWORDS): return set(w for w in words if w in NWORDS)
+def known(words, NWORDS): return set(w for w in words if w in NWORDS)
 
 
-# 如果known(set)非空, candidate 就会选取这个集合, 而不继续计算后面的
-def correct(word,NWORDS):
-    candidates = known([word],NWORDS) or known(edits1(word),NWORDS)
+def correct(word, NWORDS):
+    # 如果 known([word],NWORDS) 非空, candidate 就会选取这个集合, 而不继续计算后面的
+    candidates = known([word], NWORDS) or known(edits1(word), NWORDS)
     # 找出抽取词语中 出现次数最多的 词语
     return max(candidates, key=lambda w: NWORDS[w])
 
+
 def main():
     NWORDS = train(words(open('big.txt').read()))
-    print(correct("chan",NWORDS))
+    c=edits1("chas")
+    print(correct("chas", NWORDS))
 
 
 if __name__ == "__main__":
