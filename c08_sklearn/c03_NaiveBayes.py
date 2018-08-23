f96b98a6349015d831ff8a1ec37cfa8f3f576dd5
keyuquan <1242504517@qq.com>
2018-08-23 11:05:50 +0800

    python 自带机器学习库 sklearn GaussianNB

---
+++ c08_sklearn/c03_NaiveBayes.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+
+# 加载iris数据集
+iris = datasets.load_iris()
+
+X = iris.data
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+clf = GaussianNB()
+
+# 训练模型
+clf.fit(X_train, y_train)
+
+# 预测结果
+ans = clf.predict(X_test)
+
+# 计算准确率
+cnt = 0
+for i in range(len(y_test)):
+    if ans[i] - y_test[i] < 1e-1:
+        cnt += 1
+
+print("Accuracy: ", (cnt * 100.0 / len(y_test)), "%")
