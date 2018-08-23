5f32574583829de4ac98a4b670dcaf85947f1a75
keyuquan <1242504517@qq.com>
2018-08-23 10:50:42 +0800

    python 自带机器学习库 sklearn

--- c08_sklearn/c01_knn.py
+++ c08_sklearn/c01_knn.py
@@ -12,26 +12,21 @@ from sklearn import neighbors, datasets
 # 取最近15个点
 n_neighbors = 20
 
-# import some data to play with
 iris = datasets.load_iris()
 
 # 取2个维度的数据
 X = iris.data[:, :2]
 y = iris.target
 
-h = .01  # step size in the mesh
+h = .01
 
-# Create color maps
 cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
 cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 
+# weights： 确定近邻的权重，“uniform”权重一样，“distance”指权重为距离的倒数
 for weights in ['uniform', 'distance']:
-    # we create an instance of Neighbours Classifier and fit the data.
     clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
     clf.fit(X, y)
-
-    # Plot the decision boundary. For that, we will assign a color to each
-    # point in the mesh [x_min, x_max]x[y_min, y_max].
     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
